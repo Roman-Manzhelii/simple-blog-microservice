5ab45d569a30566d40d18fe58da60b926c145ca7
Roman Manzhelii <145562170+Roman-Manzhelii@users.noreply.github.com>
2025-09-10 22:47:48 +0100

    'client' creates post using 'post' microservice

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import PostCreate from "./PostCreate";
+export function App() {
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+        </div>
+    );
+}
+
+export default App;
