648a6565093f39616b972b195854beb696956ca8
Roman Manzhelii <145562170+Roman-Manzhelii@users.noreply.github.com>
2025-09-14 19:35:42 +0100

    fix axios 'event' request

--- comments/index.js
+++ comments/index.js
@@ -50,7 +50,7 @@ app.post('/events', async (req, res) => {
 
     comment.status = status;
 
-    await axios.post('http://localhost:4005', {
+    await axios.post('http://localhost:4005/events', {
       type: 'CommentUpdated',
       data: {
         id, status, postId, content
